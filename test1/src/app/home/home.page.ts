982ec73701e87ab17bced16bbdc9c6e0cee4fc4f
Jesús Moyano Ruz <jesus@sopinet.com>
2019-04-15 13:58:50 +0200

    first steps

--- test1/src/app/home/home.page.ts
+++ test1/src/app/home/home.page.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  item = {
+    src: "http://img.europapress.es/fotoweb/fotonoticia_20161129104141_640.jpg",
+    alt: "Obi Wan Kenobi"
+  };
 
+  funcionPrueba () {
+    this.item = 3;
+  }
+  funcionPrueba2 (): number {
+    let numero: number;
+    numero = 3;
+    return numero;
+  }
 }
